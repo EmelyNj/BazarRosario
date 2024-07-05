a7c0b26dece4ca7df555b43a0a652d173b07cedf
EmelyNj <23emelynajera@gmail.com>
2024-07-05 13:42:19 -0600

    Agregar archivos de proyecto.

---
+++ BazarRosario/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BazarRosario
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ BazarRosario/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BazarRosario
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
